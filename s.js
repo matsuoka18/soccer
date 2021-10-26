cfbe3cd8db597d83c6b7b03302423742b59ec850
matsuoka18 <82459435+matsuoka18@users.noreply.github.com>
2021-10-27 08:03:34 +0900

    Add files via upload

---
+++ s.js
@@ -0,0 +1,30 @@
+let text1 = document.getElementById('text1');
+let text2 = document.getElementById('text2');
+let text3 = document.getElementById('text3');
+let button = document.getElementById('bution');
+button.addEventListener('click',check);
+
+function check(){
+if(text1.value === "佐藤雅彦"){
+check2();
+}else{
+alert("違います");
+document.textt.reset();
+}
+}
+function check2(){
+if(text2.value === "3833"){
+check3();
+}else{
+alert("違います");
+document.textt.reset();
+}
+}
+function check3(){
+if(text3.value === "高梨"){
+
+}else{
+alert("違います");
+document.textt.reset();
+}
+};
